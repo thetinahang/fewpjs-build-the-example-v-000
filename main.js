95d618764127e39d53a576fb8ef7d20dce0ce080
Tina Hang <mstinahang@gmail.com>
2020-01-12 23:50:12 +0000

    Done.

--- main.js
+++ main.js
@@ -4,8 +4,39 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const likeGlyph = document.querySelectorAll('.like-glyph')
 
-
+for (const heart of likeGlyph) {
+  heart.addEventListener('click', (event) => {
+    console.log(event.target)
+    const heartImage = event.target
+    // invoke mimicServerCall to simulate making a server request
+    mimicServerCall()
+    // success status -> full heart
+    // add activated-heart to make the heart appear red
+    .then(() => {
+      if (heartImage.textContent !== FULL_HEART) {
+        heartImage.textContent = FULL_HEART;
+      } else {
+        heartImage.textContent = EMPTY_HEART;
+      }
+      heartImage.classList.toggle("activated-heart");
+    })
+    // randomly fails to simulate faulty network conditions
+    .catch((error) => {
+      document.getElementById('modal-message').textContent = error;
+      // display the error modal by removing the .hidden class
+      function displayError() {
+        document.getElementById('modal').classList.toggle('hidden');
+      }
+      displayError();
+      // use setTimeout to hide the modal after 5 seconds
+      const temporaryDisply = setTimeout(() => {
+        displayError();
+      }, 5000)
+    });
+  })
+}
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
